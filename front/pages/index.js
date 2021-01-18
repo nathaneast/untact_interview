cba767f868e68a714ea825b08a3e190de282a7c8
nathaneast <ahrl_ahrl@naver.com>
2021-01-18 22:53:32 +0900

    Complete: Home styles Conti

--- front/pages/index.js
+++ front/pages/index.js
@@ -3,14 +3,132 @@ import { END } from 'redux-saga';
 import axios from 'axios';
 import styled from 'styled-components';
 
+import { ButtonNavy } from '../styles/reStyled';
 import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
 import AppLayout from '../components/AppLayout';
 import wrapper from '../store/configureStore';
 
+const Container = styled.div`
+  margin: 50px 0px 20px 0px;
+`;
+
+const IntroContents = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Title = styled.div`
+  & h1 {
+    font-size: 40px;
+    color: #222f3e;
+  }
+`;
+
+const IntroWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 50px 0px;
+`;
+
+const Message = styled.p`
+  color: #222f3e;
+  font-weight: bolder;
+  font-size: 18px;
+`;
+
+const ButtonWrapper = styled.div`
+  text-align: center;
+  margin: 10px;
+  border-radius: 10px;
+  & a {
+    color: #FFFFF6;
+  }
+`;
+
+const Button = styled(ButtonNavy)`
+`;
+
+const OutroMessage = styled.div`
+  margin: 15px 0px;
+`;
+
+const ManualBoard = styled.main`
+  display: grid;
+  grid-gap: 60px;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 20px 0px;
+  & div {
+    width: 550px;
+    height: 400px;
+    background-color: green;
+    border-radius: 15px;
+  }
+`;
+
+const ManualItem = styled.div`
+  & div {
+    color: black;
+    width: 550px;
+    height: 400px;
+    background-color: green;
+    border-radius: 15px;
+  }
+  p {
+    color: black;
+    text-align: center;
+  }
+`;
+
 const Home = () => (
   <AppLayout>
-    <h1>홈</h1>
-    <button>시작하기</button>
+    <Container>
+      <IntroContents>
+        <Title>
+          <h1>Untact Interview</h1>
+        </Title>
+
+        <IntroWrapper>
+          <Message>
+            인터뷰를 진행하고 <br /> 피드백을 통해 <br /> 부족한 점을 보완해가며{' '}
+            <br /> 성장하세요
+          </Message>
+        </IntroWrapper>
+
+        <ButtonWrapper>
+          <Button>
+            <a href="/interviews">
+              <span>시작하기</span>
+            </a>
+          </Button>
+        </ButtonWrapper>
+
+        <OutroMessage>
+          <Message>당신의 성장을 응원합니다 😉👍</Message>
+        </OutroMessage>
+      </IntroContents>
+
+      <ManualBoard>
+        <ManualItem>
+          <div>이미지 1</div>
+          <p>설명 1</p>
+        </ManualItem>
+        <ManualItem>
+          <div>이미지 2</div>
+          <p>설명 2</p>
+        </ManualItem>
+        <ManualItem>
+          <div>이미지 3</div>
+          <p>설명 3</p>
+        </ManualItem>
+        <ManualItem>
+          <div>이미지 4</div>
+          <p>설명 4</p>
+        </ManualItem>
+      </ManualBoard>
+    </Container>
   </AppLayout>
 );
 
